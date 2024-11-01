8de3be0287410209c36a9147010277b389750e6b
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 09:46:46 +0100

    Cell implement

--- Jeu_de_la_vie/Models/Cell.cs
+++ Jeu_de_la_vie/Models/Cell.cs
@@ -1,6 +1,21 @@
 ﻿namespace Jeu_de_la_vie.Models;
 
-public class Cell
+public class Cell : ICell
 {
-    
+    public bool IsAlive { get; set; }          // Indique si la cellule est vivante ou morte
+    public int X { get; }                      // Position X de la cellule
+    public int Y { get; }                      // Position Y de la cellule
+
+    public Cell(int x, int y, bool isAlive = false)
+    {
+        X = x;
+        Y = y;
+        IsAlive = isAlive;
+    }
+
+    // Méthode pour inverser l'état de la cellule
+    public void ToggleState()
+    {
+        IsAlive = !IsAlive;
+    }
 }
