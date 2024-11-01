668eafdded688f9e258d822ed467b596679775c7
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 12:08:45 +0100

    Cell view

---
+++ Jeu_de_la_vie/Views/BoolToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Jeu_de_la_vie.Views;
+
+public class BoolToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isAlive = (bool)value;
+        return isAlive ? Brushes.Black : Brushes.White;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Jeu_de_la_vie/Views/CellView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Jeu_de_la_vie.Views;
+
+public partial class CellView : UserControl
+{
+    public CellView()
+    {
+        InitializeComponent();
+    }
+}
