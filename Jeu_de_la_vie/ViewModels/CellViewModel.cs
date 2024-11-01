57c36984ecfb79219c53c0c1530f2f9498fe93ce
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 10:07:49 +0100

    CellViewModel implement

--- Jeu_de_la_vie/ViewModels/CellViewModel.cs
+++ Jeu_de_la_vie/ViewModels/CellViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace Jeu_de_la_vie.ViewModels;
+﻿using System.ComponentModel;
+using Jeu_de_la_vie.Models;
 
-public class CellViewModel
+namespace Jeu_de_la_vie.ViewModels;
+
+public class CellViewModel : ICellViewModel
 {
-    
+    private readonly ICell cell;         // Modèle de la cellule
+
+    public CellViewModel(ICell cell)
+    {
+        this.cell = cell;
+    }
+
+    public bool IsAlive
+    {
+        get => cell.IsAlive;
+        set
+        {
+            if (cell.IsAlive != value)
+            {
+                cell.IsAlive = value;
+                OnPropertyChanged(nameof(IsAlive));
+            }
+        }
+    }
+
+    public void ToggleState() => cell.ToggleState();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
---
+++ Jeu_de_la_vie/ViewModels/ICellViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Jeu_de_la_vie.ViewModels;
+
+public interface ICellViewModel
+{
+    bool IsAlive { get; set; }           // État de la cellule
+    void ToggleState();                  // Change l'état de la cellule (vivante/morte)
+}
