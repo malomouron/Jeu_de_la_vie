c8466fc6b896d7c2dbe1a92ab0a2cf766357b814
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 09:54:12 +0100

    TimerService implement

--- Jeu_de_la_vie/Services/ITimerService.cs
+++ Jeu_de_la_vie/Services/ITimerService.cs
@@ -1,6 +1,11 @@
-﻿namespace Jeu_de_la_vie.Services;
+﻿using System;
+
+namespace Jeu_de_la_vie.Services;
 
 public interface ITimerService
 {
-    
+    event EventHandler Tick;                 // Événement déclenché à chaque intervalle de temps
+    void Start();                            // Démarre le timer
+    void Stop();                             // Arrête le timer
+    int Interval { get; set; }               // Intervalle de temps entre chaque Tick
 }
--- Jeu_de_la_vie/Services/TimerService.cs
+++ Jeu_de_la_vie/Services/TimerService.cs
@@ -1,6 +1,27 @@
-﻿namespace Jeu_de_la_vie.Services;
+﻿using System;
+using System.Timers;
 
-public class TimerService
+namespace Jeu_de_la_vie.Services;
+
+public class TimerService : ITimerService
 {
-    
+    private readonly Timer timer;
+
+    public TimerService(int interval)
+    {
+        timer = new Timer(interval);
+        timer.Elapsed += (sender, e) => Tick?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler Tick;
+
+    public int Interval
+    {
+        get => (int)timer.Interval;
+        set => timer.Interval = value;
+    }
+
+    public void Start() => timer.Start();
+
+    public void Stop() => timer.Stop();
 }
