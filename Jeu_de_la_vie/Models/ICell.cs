36495d931c607d73fde65e1986e916337e402acc
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 09:44:02 +0100

    ICell implement

--- Jeu_de_la_vie/Models/ICell.cs
+++ Jeu_de_la_vie/Models/ICell.cs
@@ -2,5 +2,8 @@
 
 public interface ICell
 {
-    
+    bool IsAlive { get; set; }     // Indique si la cellule est vivante ou morte
+    void ToggleState();             // Change l'état de la cellule (vivante/morte)
+    int X { get; }                  // Position X de la cellule
+    int Y { get; }                  // Position Y de la cellule
 }
