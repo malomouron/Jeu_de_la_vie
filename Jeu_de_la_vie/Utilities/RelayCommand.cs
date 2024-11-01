4466da361a61caaf22a4b27221a4b22004f00e43
Malo Mouron <malo.mouron@gmail.com>
2024-11-01 10:05:21 +0100

    RelayCommand

--- Jeu_de_la_vie/Utilities/RelayCommand.cs
+++ Jeu_de_la_vie/Utilities/RelayCommand.cs
@@ -1,6 +1,23 @@
-﻿namespace Jeu_de_la_vie.Utilities;
+﻿using System;
 
-public class RelayCommand
+namespace Jeu_de_la_vie.Utilities;
+
+public class RelayCommand 
 {
-    
+    private readonly Action<object> execute;
+    private readonly Predicate<object> canExecute;
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+    {
+        this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+
+    public void Execute(object parameter) => execute(parameter);
+
+    public event EventHandler CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 }
